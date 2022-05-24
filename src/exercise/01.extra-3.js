d6c9a3f67f47415728b8293a9a96216133df27e8
mikaWorku <mikias27worku@gmail.com>
2022-05-24 16:46:38 +0300

    useReducer excercise done

---
+++ src/exercise/01.extra-3.js
@@ -0,0 +1,24 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+const countReducer = (state, action)=>{
+  return {...state, ...(typeof action === 'function' ? action(state): action)}
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () =>
+  setState(currentState => ({count: currentState.count + step}))
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
